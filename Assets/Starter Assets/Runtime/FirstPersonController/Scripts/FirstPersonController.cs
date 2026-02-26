c4ea99dcfea55fef9f0eabd5aa06d5057a510720
LlucVelazquez <lluc.velazquez.7e8@itb.cat>
2026-02-26 16:40:42 +0100

    fixed scripts conflict (shoot behaviour pending to be fixed)

--- Assets/Starter Assets/Runtime/FirstPersonController/Scripts/FirstPersonController.cs
+++ Assets/Starter Assets/Runtime/FirstPersonController/Scripts/FirstPersonController.cs
@@ -69,8 +69,8 @@ namespace StarterAssets
 		private PlayerInput _playerInput;
 #endif
 		private CharacterController _controller;
-		private StarterAssetsInputs _input;
-		private GameObject _mainCamera;
+		private StarterAssetsInputs _input; //
+        private GameObject _mainCamera;
 
 		private const float _threshold = 0.01f;
 
@@ -98,7 +98,7 @@ namespace StarterAssets
 		private void Start()
 		{
 			_controller = GetComponent<CharacterController>();
-			_input = GetComponent<StarterAssetsInputs>();
+			_input = GetComponent<StarterAssetsInputs>(); //
 #if ENABLE_INPUT_SYSTEM
 			_playerInput = GetComponent<PlayerInput>();
 #else
